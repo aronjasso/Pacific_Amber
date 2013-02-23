e89a6622c2759807af48025a7ff1b3f46b7dda3e
aronjasso <aron.jasso@gmail.com>
2013-02-23 13:23:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PacificAmber::Application.config.secret_token = 'c3bccfa9f7339172fafcbd850190b3e07e5b2529497183bf75a5919ac88c69700d48cc7668028652f3bf5f5c6a8f5f1653b71dbae8fd3b39191019d432496676'
